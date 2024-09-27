1211677d9c556d68a0e896dd1cd9c00bbe9500e0
riversunlight <aabbee107@icloud.com>
2024-09-27 15:19:43 +0900

    ゲームシーンのスコアとレベルを追加

--- scenes/game.rb
+++ scenes/game.rb
@@ -1,12 +1,25 @@
 class Game
   def initialize
+    reset
+  end
 
+  def reset
+    @score = 0
+    @level = 1
   end
 
   def do
-    Window.draw_font(0, 0, "Game", $font30)
+    if Input.key_push?(K_ESCAPE)
+      return :title, nil
+    end
+
+    Window.draw_font(0, 0, "レベル: #{@level}", $font30)
+    Window.draw_font(0, 30, "スコア: #{@score}", $font30)
+
     if Input.key_push?(K_RETURN)
-      return :result, nil
+      res = [@level, @score]
+      reset
+      return :result, res
     end
     return :game, nil
   end
