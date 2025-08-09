ab6dde3e97ae0a1fb09723d1428d39d981faf014
riversunlight <aabbee107@icloud.com>
2025-08-09 22:01:27 +0900

    終了メッセージのバグ修正

--- scenes/game.rb
+++ scenes/game.rb
@@ -8,7 +8,8 @@ class Game
     @level = 0
     @targets = []
     @start_time = nil
-    @limit_time = 10
+    @limit_time = 60
+
   end
 
   # 的の生成
@@ -21,13 +22,47 @@ class Game
       @targets << Target.new(base_x: 100, base_y:200, move_kind: :nomove, point: 10)
       @targets << Target.new(base_x: 200, base_y:300, move_kind: :nomove, point: 10)
     when 3
+      @targets << Target.new(base_x: 300, base_y:200, move_kind: :nomove, point: 10)
+      @targets << Target.new(base_x: 200, base_y:450, move_kind: :nomove, point: 10)
+      @targets << Target.new(base_x: 550, base_y:400, move_kind: :nomove, point: 10)
     when 4
+      4.times do
+        @targets << Target.new(base_x: Random.rand(250 .. 350), base_y:Random.rand(250 .. 450), move_kind: :nomove, point: 10)
+      end
     when 5
+      4.times do
+        @targets << Target.new(base_x: Random.rand(100 .. 550), base_y:Random.rand(200 .. 450), move_kind: :nomove, point: 10)
+      end
     when 6
+      @targets << Target.new(base_x: 200, base_y: 300, move_kind: :circle, point: 10, speed: 4, radius:100)
     when 7
+      @targets << Target.new(base_x: 200, base_y: 100, move_kind: :warp, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 100, base_y: 200, move_kind: :warp, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 200, base_y: 300, move_kind: :warp, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 300, base_y: 200, move_kind: :warp, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 400, base_y: 400, move_kind: :warp, point: 10, speed: 10, radius:100)
+
     when 8
+      @targets << Target.new(base_x: 300, base_y: 200, move_kind: :xpendulum, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 300, base_y: 400, move_kind: :xpendulum, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 50, base_y: 300, move_kind: :ypendulum, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 500, base_y: 300, move_kind: :ypendulum, point: 10, speed: 10, radius:100)
     when 9
+      @targets << Target.new(base_x: 200, base_y: 200, move_kind: :nomove, point: 10, speed: 10)
+      @targets << Target.new(base_x: 100, base_y: 300, move_kind: :nomove, point: 10, speed: 10)
+
+      @targets << Target.new(base_x: 300, base_y: 300, move_kind: :warp, point: 10, speed: 20, radius:100)
+      @targets << Target.new(base_x: 500, base_y: 400, move_kind: :warp, point: 10, speed: 10, radius:100)
+
+      @targets << Target.new(base_x: 300, base_y: 300, move_kind: :xpendulum, point: 10, speed: 10, radius:100)
+      @targets << Target.new(base_x: 300, base_y: 300, move_kind: :ypendulum, point: 10, speed: 10, radius:100)
+
+      @targets << Target.new(base_x: 300, base_y: 300, move_kind: :circle, point: 10, speed: 10, radius:100)
     when 10
+      @targets << Target.new(base_x: 300, base_y: 300, move_kind: :square, point: 10, speed: 1, radius:100)
+    when 11
+      #TODO: 制限時間的に動かないのを修正
+      @level = 1
     end
   end
 
@@ -35,10 +70,13 @@ class Game
   # sleep 2ではなく、メインループから戻りながら2秒待機するだけでよい
   def end_view
     Window.draw_font(100, 100, "終了", $font90)
+    return :game,  nil
+  end
+
+  def end_game
     res = [@level, @score]
-    sleep 2
     reset
-    return :result,  res
+    return :result, res
   end
 
   def do(args = {})
@@ -51,9 +89,12 @@ class Game
 
     Window.draw_font(0, 0, "レベル: #{@level}", $font30)
     Window.draw_font(0, 30, "スコア: #{@score}", $font30)
-    Window.draw_font(300, 0, "残り時間: #{(@limit_time - passed_time).to_i}秒", $font30)
+    Window.draw_font(300, 0, "残り時間: #{[(@limit_time - passed_time).to_i, 0].max}秒", $font30)
 
     if passed_time > @limit_time
+      if passed_time > @limit_time + 2
+        return end_game
+      end
       return end_view
     end
 
@@ -63,6 +104,7 @@ class Game
     end
 
     @targets.each do |target|
+      target.move
       target.draw
     end
 
--- utility/target.rb
+++ utility/target.rb
@@ -7,6 +7,9 @@ class Target
     @point = args[:point]
     @x = @base_x
     @y = @base_y
+    @time = args[:start_time] || 0
+    @radius = args[:radius] || 30
+    @speed = args[:speed] || 1
   end
 
   def draw
@@ -17,6 +20,42 @@ class Target
     case @move_kind
     when :nomove
       # No Move
+    when :circle
+      @time += @speed
+      @x = @base_x + @radius * Math.cos(@time/100.0)
+      @y = @base_y + @radius * Math.sin(@time/100.0)
+    when :xpendulum
+      @x = @base_x + @radius * Math.sin(@time/100.0)
+      @y = @base_y
+      @time += @speed
+    when :ypendulum
+      @x = @base_x
+      @y = @base_y + @radius * Math.sin(@time/100.0)
+      @time += @speed
+    when :square
+      @time += @speed
+      k = @time / 100
+      if k == 0
+        @x = @base_x + (@radius * 2 / 100.0) * @time
+        @y = @base_y + @radius
+      elsif k == 1
+        @x = @base_x + @radius
+        @y = @base_y - (@radius * 2 / 100.0) * @time
+      elsif k == 2
+        @x = @base_x - (@radius * 2 / 100.0) * @time
+        @y = @base_y - @radius
+      elsif k == 3
+        @x = @base_x + @radius
+        @y = @base_y + (@radius * 2 / 100.0) * @time
+      end
+      @time %= 400
+    when :warp
+      @time += @speed
+      if @time >= 1000
+        @time = 0
+        @x = @base_x + Random.rand(-@radius .. @radius)
+        @y = @base_y + Random.rand(-@radius .. @radius)
+      end
     end
   end
 
